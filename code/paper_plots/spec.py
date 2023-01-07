276cfdbd4359a185f83bf864ebaf099ee600d18a
Anna Ho <annayqho@gmail.com>
2023-01-07 17:30:35 -0500

    func to plot lines

--- code/paper_plots/spec.py
+++ code/paper_plots/spec.py
@@ -13,7 +13,6 @@ from fit_spectrum import *
 def main_spec(ax, wl, flam):
     """ The main spectrum panel """
 
-
     # Plot
     offset = 2
     ax.step(wl, flam/1E-16+offset, where='mid', c='k', lw=0.5)#, alpha=0.5)
@@ -49,10 +48,16 @@ def panels(ax, wl_range, wl, flam):
     plt.yticks([])
 
 
+def plot_lines(ax, species):
+    for l in wl_lines[species]:
+        ax.axvline(l, lw=0.5, ls=':')
+
+
 if __name__=="__main__":
     # Get data
     wl, flam, eflam = load_spec_1()
     wl = wl/(1+vals.z)
+    wl_lines = get_rest_wl()
 
     # Initialize figure
     fig = plt.figure(figsize=(6,5))
@@ -61,28 +66,27 @@ if __name__=="__main__":
     ax = fig.add_subplot(gs[1, :])
     main_spec(ax, wl, flam)
 
-    # Zoom-in of the left-most line
+    # Zoom-in of the OII (left-most) line
     ax = fig.add_subplot(gs[0, 0])
     panels(ax, [3700, 3760], wl, flam)
+    plot_lines(ax, 'oii')
 
     # Zoom-in of the middle lines
     ax = fig.add_subplot(gs[0, 1])
     panels(ax, [4800, 5050], wl, flam)
+    plot_lines(ax, 'oiii')
 
     # Zoom-in of the right lines
     ax = fig.add_subplot(gs[0, 2])
     panels(ax, [6530, 6770], wl, flam)
 
     # Mark lines
-    #lines = get_rest_wl()
     #for key,line in lines.items():
     #    for l in line:
     #        ax.axvline(x=l, lw=1, alpha=0.3, c='grey')
 
     # wl_lines['ha'] = [6562.819]
     # wl_lines['hb'] = [4861.333]
-    # wl_lines['oii'] = [3726.032, 3728.815]
-    # wl_lines['oiii'] = [4958.911, 5006.843]
     # wl_lines['nii'] = [6548.050, 6583.460]
     # # only fit the first line...the second one has nans in it
     # wl_lines['sii'] = [6716.440]#, 6730.810]
