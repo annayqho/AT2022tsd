079ed4ef08e4edbb4ea7a6a0e6f96e75cb5fafc4
Anna Ho <annayqho@gmail.com>
2023-01-25 10:48:11 -0500

    add flux and luminosity

--- code/paper_tables/print_xray.py
+++ code/paper_tables/print_xray.py
@@ -5,7 +5,7 @@ from astropy.time import Time
 sys.path.append("/Users/annaho/Dropbox/astro/papers/papers_active/AT2022tsd/code")
 from get_opt import get_ipac
 from get_radio_at2022tsd import *
-from get_xray import load_swift
+from get_xray import *
 import vals
 
 def print_table():
@@ -16,8 +16,8 @@ def print_table():
             ['t', 't_\mathrm{err}', '$\Delta t$\\footnote{Rest frame}', 
              '$\Delta T$', 'Count Rate', '$F_X$', '$L_X$', 'Telescope'])
     unit_headings = np.array(
-            ['(UT)', '(days)', '(days)', '(ksec)', '(s$^{-1}$)', 
-             '(erg\,s$^{-1}$\,cm$^{-2}$)', '$(10^{43}$\,erg\,s$^{-1}$)', ''])
+            ['(UT)', '(days)', '(days)', '(ksec)', '($10^{-3}$\,s$^{-1}$)', 
+             '$10^{-14}$ (erg\,s$^{-1}$\,cm$^{-2}$)', '$(10^{43}$\,erg\,s$^{-1}$)', ''])
     label = "tab:xray-observations"
 
     ncol = len(headings)
@@ -64,21 +64,31 @@ def print_table():
         # Effective time...only want minute precision
         tstr = str((t-et).isot).replace('T', ' ').split('.')[0][0:-3]
         # Error on the time
-        texp = '{:.2f}'.format(et)
+        terror = '{:.2f}'.format(et)
         # Rest-frame days
         dtstr = '{:.2f}'.format((t.jd-vals.t0)/(1+vals.z))
         # Exposure time
+        texp = '{:.2f}'.format(get_exp(i+1)/1000)
 
-        # Luminosity formatting
-        L = '{:.2f}'.format(Ls[i]/1E43)
-        lL = '{:.2f}'.format(lLs[i]/1E43)
-        uL = '{:.2f}'.format(uLs[i]/1E43)
-        if lL==uL:
-            Lstr = "$%s\pm%s$" %(L,lL)
-        else:
-            Lstr = "$%s_{-%s}^{+%s}$" %(L,lL,uL)
+        # Counts
+        ct = '{:.2f}'.format(df['Rate    '][i]/1E-3)
+        # Errors are symmetric
+        ect = '{:.2f}'.format(df['Ratepos '][i]/1E-3)
+        ctstr = "$%s\pm%s$" %(ct,ect)
 
-        row = rowstr %(tstr,dtstr,"","",Lstr,'Swift')
+        # Flux
+        f = '{:.2f}'.format(df['Flux'][i]/1E-14)
+        # Errors are symmetric
+        ef = '{:.2f}'.format(df['Fluxpos'][i]/1E-14)
+        fstr = "$%s\pm%s$" %(ct,ect)
+
+        # Luminosity
+        L = '{:.2f}'.format(df['L'][i]/1E43)
+        # Errors are symmetric
+        eL = '{:.2f}'.format(df['Lpos'][i]/1E43)
+        Lstr = "$%s\pm%s$" %(ct,ect)
+
+        row = rowstr %(tstr,terror,dtstr,texp,ctstr,fstr,Lstr,'Swift/XRT')
         print(row)
         outputf.write(row)
 
