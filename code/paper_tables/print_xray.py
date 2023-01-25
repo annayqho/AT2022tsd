f4031dab231c82cf990082cce727c72c8d0f0ae6
Anna Ho <annayqho@gmail.com>
2023-01-25 11:08:35 -0500

    minor changes

--- code/paper_tables/print_xray.py
+++ code/paper_tables/print_xray.py
@@ -13,11 +13,11 @@ def print_table():
 
     # Headings
     headings = np.array(
-            ['t', 't_\mathrm{err}', '$\Delta t$\\footnote{Rest frame}', 
-             '$\Delta T$', 'Count Rate', '$F_X$', '$L_X$', 'Telescope'])
+            ['$t$', '$\Delta t$\\footnote{Rest frame}', 
+             '$t_\mathrm{exp}$', 'Count Rate', '$F_X$', '$L_X$', 'Tel.'])
     unit_headings = np.array(
-            ['(UT)', '(days)', '(days)', '(ksec)', '($10^{-3}$\,s$^{-1}$)', 
-             '$10^{-14}$ (erg\,s$^{-1}$\,cm$^{-2}$)', '$(10^{43}$\,erg\,s$^{-1}$)', ''])
+            ['(UT)', '(days)', '(ksec)', '($10^{-3}$\,s$^{-1}$)', 
+             '($10^{-14}$ erg\,s$^{-1}$\,cm$^{-2}$)', '$(10^{43}$\,erg\,s$^{-1}$)', ''])
     label = "tab:xray-observations"
 
     ncol = len(headings)
@@ -44,10 +44,9 @@ def print_table():
     caption="X-ray observations of AT2022tsd."
 
     outputf = open("paper_table_%s.txt" %label, "w")
-    outputf.write("\\begin{center} \n")
-    outputf.write("\\begin{longtable}{%s} \n" %colstr)
-    outputf.write("\\caption{%s} \n" %caption)
-    outputf.write("\\label{%s}\\\ \n" %label)
+    outputf.write("\\begin{table*} \n")
+    outputf.write("\\centering \n")
+    outputf.write("\\begin{tabular}{%s}\n" %colstr)
     outputf.write("\hline\hline\n")
     outputf.write(colheadstr+'\\\ \n')
     outputf.write(unitstr+'\\\ \n')
@@ -56,17 +55,18 @@ def print_table():
     # Error bars are asymmetric
     df = load_swift()
 
-    for i in np.arange(len(t)):
+    for i in np.arange(len(df)):
         # Convert date to readable date and time
         t = Time(df['!MJD    '][i], format='mjd')
         et = df['T_+ve   '][i]
 
         # Effective time...only want minute precision
         tstr = str((t-et).isot).replace('T', ' ').split('.')[0][0:-3]
-        # Error on the time
-        terror = '{:.2f}'.format(et)
         # Rest-frame days
-        dtstr = '{:.2f}'.format((t.jd-vals.t0)/(1+vals.z))
+        dt = '{:.2f}'.format((t.jd-vals.t0)/(1+vals.z))
+        # Error on the time
+        terror = '{:.2f}'.format(et/(1+vals.z))
+        dtstr = '$%s\pm%s$' %(dt,terror)
         # Exposure time
         texp = '{:.2f}'.format(get_exp(i+1)/1000)
 
@@ -88,14 +88,16 @@ def print_table():
         eL = '{:.2f}'.format(df['Lpos'][i]/1E43)
         Lstr = "$%s\pm%s$" %(ct,ect)
 
-        row = rowstr %(tstr,terror,dtstr,texp,ctstr,fstr,Lstr,'Swift/XRT')
+        row = rowstr %(tstr,dtstr,texp,ctstr,fstr,Lstr,'Swift')
         print(row)
         outputf.write(row)
 
     outputf.write("\hline \n")
+    outputf.write("\\end{tabular} \n")
+    outputf.write("\\caption{%s} \n" %caption)
+    outputf.write("\\label{%s} \n" %label)
 
-    outputf.write("\end{longtable} \n")
-    outputf.write("\end{center} \n")
+    outputf.write("\end{table*} \n")
     outputf.close()
 
 
