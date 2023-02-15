9a5852f7f7c1b61b5f1638c9d8ff34cd1f9ea70e
Anna Ho <annayqho@gmail.com>
2023-02-15 23:45:45 +0100

    delete redundant funcs

--- code/paper_plots/flare_collage.py
+++ code/paper_plots/flare_collage.py
@@ -54,174 +54,13 @@ def plot_ultraspec_panel(ax, dat, t0, filt, m, col, plot_binned=False):
                 lw=0.5)
 
 
-def plot_keck(ax, dat, t0, flare_num=1):
-    """ Plot a light-curve panel """
-    t0_mjd = float(Time(t0, format='isot').mjd)
-    m = 5
-
-    # Get the relevant data
-    choose = dat['flt']=='u'
-    x = dat['mjdstart'][choose].values
-    y = dat['flux'][choose].values
-    ey = dat['unc'][choose].values
-
-    # Plot in minutes
-    dt = (x-t0_mjd)*24*60
-
-    # Plot detections
-    ax.errorbar(dt, y, ey, c=vals.uc, fmt='*', ms=m*2, lw=0.5, label='$u$')
-
-    # Get the relevant data
-    choose = dat['flt']=='i'
-    x = dat['mjdstart'][choose].values
-    y = dat['flux'][choose].values
-    ey = dat['unc'][choose].values
-
-    # Plot in minutes
-    dt = (x-t0_mjd)*24*60
-
-    # Plot detections
-    ax.errorbar(dt, y, ey, c=vals.ic, fmt='D', ms=m, lw=0.5, label='$i$')
-
-    # Format x-axis
-    t0_str = t0[0:16].replace('T', ' ')
-    ax.set_xlabel("Min. since %s" %t0_str, fontsize=8)
-
-    ax.axhline(y=0, c='grey', lw=0.5)
-    ax.legend(loc='upper right', ncol=1, fontsize=8)
-    ax.set_ylim(-0.5,2.7)
-
-
-def plot_magellan(ax):
-    """ Plot the Magellan/IMACS flare in uJy """
-    dat = get_non_ztf()
-    choose = dat['#inst']=='Magella'
-    mag = dat['mag']
-    emag = dat['emag']
-    mjd = dat['mjdstart']
-    y = (10**((mag[choose]-8.9)/(-2.5))) * 1E6
-    ey = y*emag[choose]*(np.log(10))/2.5
-    t0 = Time("2022-12-15T04:30:00", format='isot').mjd
-    ax.errorbar((mjd[choose]-t0)*24*60, y, ey,
-                fmt='s', c=vals.gc)
-    ax.text(0.02, 0.15, 'IMACS $g$-band', transform=ax.transAxes,
-            ha='left', va='top', fontsize=8)
-    #ax.text(0.02, 0.88, '2022-12-15', transform=ax.transAxes,
-    #        ha='left', va='top', fontsize=8)
-    ax.set_xlabel("Min. since 2022-12-15 04:30", fontsize=8)
-
-
-def plot_lt(ax):
-    dat = get_non_ztf()
-    tel = dat['#inst'].values
-    flare = dat['flare'].values
-    mag = dat['mag'].values
-    emag = dat['emag'].values
-    filt = dat['flt'].values
-
-    t0 = Time("2022-12-16T20:30:00", format='isot').mjd
-
-    mjd = dat['mjdstart'].values
-    choose = np.logical_and.reduce((tel=='LT/IO:O', flare=='**',
-                                    np.abs(mjd-t0)<1))
-    y = (10**((mag[choose]-8.9)/(-2.5))) * 1E6
-    ey = y*emag[choose]*(np.log(10))/2.5
-
-    gdet = np.logical_and(choose, filt=='g')
-    ax.errorbar((mjd[gdet]-t0)*24*60, y, ey,
-                fmt='s', c=vals.gc)
-    ax.text(0.98, 0.98, 'LT $g$-band', transform=ax.transAxes,
-            ha='right', va='top', fontsize=8)
-    ax.set_xlabel("Min. since 2022-12-16 20:30", fontsize=8)
-
-
-def plot_not(ax):
-    dat = get_non_ztf()
-    tel = dat['#inst'].values
-    flare = dat['flare'].values
-    mag = dat['mag'].values
-    emag = dat['emag'].values
-    filt = dat['flt'].values
-
-    t0 = Time("2022-12-23T01:30:00", format='isot').mjd
-
-    mjd = dat['mjdstart'].values
-    choose = np.logical_and.reduce((tel=='NOT/ALF', np.abs(mjd-t0)<1))
-    y = (10**((mag[choose]-8.9)/(-2.5))) * 1E6
-    ey = y*emag[choose]*(np.log(10))/2.5
-
-    ax.errorbar((mjd[choose]-t0)*24*60, y, ey,
-                fmt='s', c=vals.gc)
-    ax.text(0.98, 0.98, 'NOT $g$-band', transform=ax.transAxes,
-            ha='right', va='top', fontsize=8)
-    ax.set_xlabel("Min. since 2022-12-23 01:30", fontsize=8)
-
-
-def plot_ztf(ax, flarenum=1, window=1):
-    """ Plot one of the ZTF flares. Can be flarenum=1 or flarenum=2
-    or flarenum=3 (there are two r-band flares and one i-band flare) 
-
-    Window represents the days on either side that you show
-    """
-
-    # Get the data
-    jd,exp,filt,mag,emag,fujy,efujy = get_ipac()
-
-    # The dates of the two flares
-    t0s = np.array(
-            ['2022-10-04T09:30:00', '2022-10-05T08:10:04.002', 
-             '2022-10-29T04:30:00'])
-    t0 = Time(t0s[flarenum-1], format='isot').jd
-
-    # Plot the r-band data from that day 
-    choose = np.logical_and.reduce((filt=='r', jd>t0-window, jd<t0+window))
-    if sum(choose)>0:
-        ax.errorbar((jd[choose]-t0), fujy[choose], efujy[choose], 
-                fmt='o', c=vals.rc, label='$r$')
-
-    # Plot the g-band data from that day 
-    choose = np.logical_and.reduce((filt=='g', jd>t0-window, jd<t0+window))
-    if sum(choose)>0:
-        ax.errorbar((jd[choose]-t0), fujy[choose], efujy[choose], 
-                    fmt='s', c=vals.gc, label='$g$')
-
-    # Plot the i-band data from that day
-    choose = np.logical_and.reduce((filt=='i', jd>t0-window, jd<t0+window))
-    if sum(choose)>0:
-        ax.errorbar((jd[choose]-t0), fujy[choose], efujy[choose], 
-                    fmt='D', c=vals.ic, label='$i$')
-
-    # Plot LT points for that first flare
-    if flarenum==1:
-        dat = get_non_ztf()
-        tel = dat['#inst'].values
-        jd = Time(dat['mjdstart'].values, format='mjd').jd
-        choose = np.logical_and.reduce((
-            tel=='LT/IO:O', dat['flt']=='g', jd>t0-window, jd<t0+window))
-        x = jd[choose]
-        y = dat['flux'][choose].values
-        ey = dat['unc'][choose].values
-        ax.errorbar((x-t0), y, ey, fmt='s', c=vals.gc)
-
-        choose = np.logical_and.reduce((
-            tel=='LT/IO:O', dat['flt']=='r', jd>t0-window, jd<t0+window))
-        x = jd[choose]
-        y = dat['flux'][choose].values
-        ey = dat['unc'][choose].values
-        ax.errorbar((x-t0), y, ey, fmt='o', c=vals.rc)
-
-    # Formatting the panel
-    t0_str = t0s[flarenum-1][0:16].replace('T', ' ')
-    return t0_str
-
-
-def plot_flare(ax, tab, mjd, window=1):
+def plot_flare(ax, tab, mjd, window=1, unit='Days'):
     """ Plot the flare from a single night 
     The window is the interval used to select data around the flare. """
-    
     # Data from the night of choice
-    choose = tab['flare_nights']==mjd
-    t0 = tab['mjdstart'][choose][tab[choose]['flare']][0]
+    choose = np.logical_and(tab['flare_nights']==mjd, tab['flare'])
+    indmax = np.argmax(tab['flux'][choose])
+    t0 = tab['mjdstart'][choose].values[indmax]
 
     # Extract relevant data
     filt = tab['flt'].values
@@ -229,16 +68,31 @@ def plot_flare(ax, tab, mjd, window=1):
     fujy = tab['flux'].values
     efujy = tab['unc'].values
 
+    # Telescope string
+    tel_list = []
+
     # Plot
     cols = [vals.rc, vals.gc, vals.ic, vals.uc]
+    syms = ['o', 's', 'D', '*']
     for i,b in enumerate(np.array(['r', 'g', 'i', 'u'])):
         choose = np.logical_and.reduce((filt==b, t>t0-window, t<t0+window))
         if sum(choose)>0:
-            ax.errorbar((t[choose]-t0), fujy[choose], efujy[choose], 
-                    fmt='o', c=cols[i], label='$%s$' %b)
+            for val in np.unique(tab['#inst'].values[choose]):
+                tel_list.append(val.split('/')[0])
+            fac = 1
+            if unit=='Hours':
+                fac = 24
+            if unit=='Minutes':
+                fac = 24*60
+            ax.errorbar((t[choose]-t0)*fac, fujy[choose], efujy[choose], 
+                    fmt=syms[i], c=cols[i], label='$%s$' %b)
 
     t0_str = str(Time(t0, format='mjd').isot).replace('T', ' ')[0:-7]
-    return t0_str
+    tel_list = np.unique(np.array(tel_list))
+    tel_str = '+'.join(tel_list)
+
+    # Telecope string
+    return t0_str,tel_str
 
 
 if __name__=="__main__":
@@ -248,70 +102,43 @@ if __name__=="__main__":
     # Get the optical photometry
     tab = get_full_opt()
 
-    # Identify flares as 5-sigma detections after the first ZTF detection
-    tab['flare'] = np.logical_and(tab['sig']>5, tab['mjdstart']>59830)
+    # Identify flares as 5-sigma detections after a chosen time
+    tab['flare'] = np.logical_and(tab['sig']>5, tab['mjdstart']>59856)
 
     # Identify nights with flares (since we'll plot each night individually)
-    flare_nights = np.unique(tab['mjdstart'][tab['flare']==True].astype(int))
+    flare_nights = np.unique(tab['mjdstart'][tab['flare']].astype(int))
     tab['flare_nights'] = tab['mjdstart'].astype(int)
 
-    # There are 11 flare nights. The two ZTF flares will be in a single
-    # row, and the ULTRASPEC flares will have their own rows.
+    # There are 10 flare nights. 
 
-    # Plot ZTF: the first r and i flares (1d apart)
-    ax = plt.subplot(4,2,1)
-    t0_str = plot_flare(ax, tab, flare_nights[0], window=2.5)
-    ax.set_ylabel(r"$f_\nu$ ($\mu$Jy)")
-    ax.axhline(y=0, c='grey', lw=0.5)
-    axins = ax.inset_axes([0.2, 0.54, 0.2, 0.42])
-    #t0_str = plot_flare(axins, window=0.1)
-    #ax.legend(loc='upper right', fontsize=8, ncol=1, columnspacing=0.2,
-    #      handletextpad=0.1)
-    ax.set_xlabel("Days since %s" %t0_str, fontsize=8)
-    #ax.text(0.01, 0.95, 'ZTF+LT', ha='left', va='top', fontsize=8,
-    #        transform=ax.transAxes)
-    axins.set_yticks([])
-    axins.set_xticks([])
-    axins.set_xlabel("15 min", fontsize=8, labelpad=1)
-    ax.indicate_inset_zoom(axins, edgecolor="grey")
+    # Some custom settings
+    windows = (np.ones(len(flare_nights))/24/60)*30
+    windows[0] = 2.5 # first ZTF night needs longer
+    units = ['Minutes']*len(flare_nights)
+    units[0] = 'Days' # for that first ZTF obs
 
-    # Plot ZTF: the second r-band flare
-    # ax = plt.subplot(4,2,2)
-    # t0_str = plot_ztf(ax, flarenum=3, window=1.5)
-    # ax.legend(loc='lower center', fontsize=8, ncol=3, columnspacing=0.2,
-    #       handletextpad=0.1)
-    # ax.set_xlabel("Days since %s" %t0_str, fontsize=8)
-    # ax.text(0.05, 0.95, 'ZTF', ha='left', va='top', fontsize=8,
-    #         transform=ax.transAxes)
-    # axins = ax.inset_axes([0.7, 0.54, 0.28, 0.45])
-    # plot_ztf(axins, flarenum=3, window=0.1)
-    # axins.set_yticks([])
-    # axins.set_xticks([])
-    # axins.set_xlabel("7 min", fontsize=8, labelpad=1)
-    # ax.indicate_inset_zoom(axins, edgecolor="grey")
+    for i,night in enumerate(flare_nights):
+        if i != 1: # skip the ZTF i-band point, already in first panel
+            if i < 1:
+                ind = i + 1
+            else:
+                ind = i
+            ax = plt.subplot(4,3,ind)
+            t0_str,tel_str = plot_flare(ax, tab, night, window=windows[i],
+                                        unit=units[i])
+            ax.text(0.01, 0.95, tel_str, ha='left', va='top', fontsize=8,
+                    transform=ax.transAxes)
+            ax.set_ylabel(r"$f_\nu$ ($\mu$Jy)")
+            ax.axhline(y=0, c='grey', lw=0.5)
+            ax.set_xlabel("%s since %s" %(units[i],t0_str), fontsize=8)
 
-    # # Plot IMACS panel
-    # ax = plt.subplot(4,4,5)
-    # plot_magellan(ax)
-    # ax.set_ylabel(r"$f_\nu$ ($\mu$Jy)")
-
-    # # Plot LT panel
-    # # So far, LT just has one flare
-    # ax = plt.subplot(4,4,6)
-    # plot_lt(ax)
-
-    # # Plot NOT panel
-    # # One flare
-    # ax = plt.subplot(4,4,7)
-    # plot_not(ax)
-
-    # # Plot Keck panel
-    # ax = plt.subplot(4,4,8)
-    # dat = get_non_ztf()
-    # plot_keck(ax, dat, '2022-12-29T10:00:00')
-    # ax.set_xlim(8, 33)
-    # ax.text(0.02, 0.02, 'LRIS', transform=ax.transAxes,
-    #         ha='left', va='bottom', fontsize=8)
+            if i==0:
+                axins = ax.inset_axes([0.2, 0.4, 0.2, 0.42])
+                t0_str,tel_str = plot_flare(axins, tab, night, window=0.2)
+                axins.set_yticks([])
+                axins.set_xticks([])
+                axins.set_xlabel("15 min", fontsize=8, labelpad=1)
+                ax.indicate_inset_zoom(axins, edgecolor="grey")
 
     # # Plot ULTRASPEC r-band panel
     # ax = plt.subplot(4,1,3)
