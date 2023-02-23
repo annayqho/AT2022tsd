ef07551089cc0569cd79260946548188b106902b
Anna Ho <annayqho@gmail.com>
2023-02-23 15:32:56 +0100

    try fitting an exponential to the LRIS data

--- code/fit_opt_flares.py
+++ code/fit_opt_flares.py
@@ -27,6 +27,15 @@ def exp(x, A, tau):
     return A*np.exp(-x/tau)
 
 
+def exp_gi(x, A, tau, fac):
+    """ exponential fit, for dual band (Keck) 
+    same number of observations in both bands """
+    xg = x[0:int((len(x)+1)/2)]
+    yg = A*np.exp(-xg/tau)
+    yi = yg*fac
+    return np.hstack((yg, yi))
+
+
 def ultraspec_gband_small(ax, return_residuals=False):
     """ Fit the small ULTRASPEC g-band flare """
     out = get_full_opt()
@@ -137,7 +146,7 @@ def ultraspec_gband_large(ax, show='exponential', return_residuals=False):
             return xflare, ygausfit-ygausmod, eyflare
 
 
-def gband():
+def ultraspec_gband():
     """ Do the analysis for the ULTRASPEC g-band light curve """
     # Initialize figure
     fig,axarr = plt.subplots(figsize=(7,7))
@@ -212,10 +221,64 @@ def gband():
 
     # Show
     plt.tight_layout()
-    plt.savefig("ultraspec_flare_fits.png", dpi=200, bbox_inches='tight', pad_inches=0.1)
+    plt.savefig(
+            "ultraspec_flare_fits.png", dpi=200, 
+            bbox_inches='tight', pad_inches=0.1)
     plt.close()
     #plt.show()
 
 
 if __name__=="__main__":
-    gband()
+    # Fit the first LRIS flare
+    out = get_full_opt()
+    dat = out[np.logical_and(out['mjdstart']>59866.5, out['mjdstart']<59873)]
+    x = (dat['mjdstart']-dat['mjdstart'].values[0])*24
+    y = dat['flux']
+    ey = dat['unc']
+
+    # Get the g-band data
+    tofit = np.logical_and(dat['flt']=='g',dat['#instrument']=='KeckI/LRIS') # exponential
+    #tofit = choose # gaussian
+    t0 = x[tofit].values[0]
+    xgfit = x[tofit]-t0
+    plt.errorbar(x[tofit]-t0, y[tofit], ey[tofit], fmt='o', c='green')
+    ygfit = y[tofit]
+    eygfit = ey[tofit]
+    plt.errorbar(xgfit, ygfit, eygfit, fmt='o', c='green')
+
+    tofit = np.logical_and(dat['flt']=='i',dat['#instrument']=='KeckI/LRIS') 
+    xifit = x[tofit]-t0
+    yifit = y[tofit]
+    eyifit = ey[tofit]
+    plt.errorbar(xifit, yifit, eyifit, fmt='o', c='goldenrod')
+
+    # Try a dual-band exponential.
+    xfit = np.hstack((xgfit, xifit)).astype(float)
+    yfit = np.hstack((ygfit, yifit)).astype(float)
+    eyfit = np.hstack((eygfit, eyifit)).astype(float)
+    p0 = [2, 0.1, 2]
+    popt, pcov = curve_fit(
+            exp_gi, xfit, yfit, sigma=eyfit, absolute_sigma=True, p0=p0)
+    xplt = np.linspace(min(xfit), max(xfit))
+    yplt = exp_gi(np.hstack((xplt,xplt)), *popt)
+    ygplt = yplt[0:int((len(yplt)+1)/2)]
+    yiplt = yplt[int((len(yplt)+1)/2):]
+    plt.plot(xplt, ygplt, c='green')
+    plt.plot(xplt, yiplt, c='yellow')
+    #print(popt, np.sqrt(pcov[1,1]))
+    # Result: 3.6, 0.50, 1.66. clearly doesn't do a good job.
+
+    # Try a Gaussian
+    #p0 = [0.66, 4.44, 1.05] # for g-band
+    #popt, pcov = curve_fit(
+    #        gauss, xgfit, ygfit, sigma=eygfit, absolute_sigma=True, p0=p0, 
+    #        maxfev=10000)
+    #xplt = np.linspace(-1,2)
+    #yg = gauss(xplt, *popt)
+    #plt.plot(xplt, yg, c='green')
+    #plt.plot(xplt, yg*2, c='yellow')
+    #print(popt)
+    #print(popt[1], np.sqrt(pcov[1,1]))
+
+
+    plt.show()
