911f4dd095e886f30dea5c117c8222070293ffb8
Anna Ho <annayqho@gmail.com>
2023-02-24 10:22:35 +0100

    add in the LT and MAgellan flares

--- code/fit_opt_flares.py
+++ code/fit_opt_flares.py
@@ -36,6 +36,46 @@ def exp_gi(x, A, tau, fac):
     return np.hstack((yg, yi))
 
 
+def pow_gi(x, A, tau, fac):
+    """ power-law fit, for dual band (Keck) 
+    same number of observations in both bands """
+    xg = x[0:int((len(x)+1)/2)]
+    yg = A*xg**(-tau)
+    yi = yg*fac
+    return np.hstack((yg, yi))
+
+
+def gauss_gi(x, sigma, A, b, fac):
+    xg = x[0:int((len(x)+1)/2)]
+    yg = A*np.exp(-(xg-b)**2/(2*sigma**2))
+    yi = yg*fac
+    return np.hstack((yg, yi[1:]))
+
+
+def gauss_const_gi(x, sigma, A, b, fac, offset):
+    xg = x[0:int((len(x)+1)/2)]
+    yg = offset+A*np.exp(-(xg-b)**2/(2*sigma**2))
+    yi = yg*fac
+    return np.hstack((yg, yi))
+
+
+def gauss_exp_gi(x, sigma, A, b, fac, B, tau):
+    xg = x[0:int((len(x)+1)/2)]
+    yg = A*np.exp(-(xg-b)**2/(2*sigma**2)) + B*np.exp(-xg/tau)
+    yi = yg*fac
+    return np.hstack((yg, yi))
+
+
+def gauss_exp(x, sigma, A, b, B, tau):
+    """ Fit a Gaussian + exponential for a single band """
+    return A*np.exp(-(x-b)**2/(2*sigma**2)) + B*np.exp(-x/tau)
+
+
+def gauss_const(x, sigma, A, b, offset):
+    """ Fit a Gaussian + constant for a single band """
+    return A*np.exp(-(x-b)**2/(2*sigma**2)) + offset
+
+
 def ultraspec_gband_small(ax, return_residuals=False):
     """ Fit the small ULTRASPEC g-band flare """
     out = get_full_opt()
@@ -130,16 +170,6 @@ def ultraspec_gband_large(ax, show='exponential', return_residuals=False):
         ax.plot(
                 xplt,yplt,lw=1.5,c='k', zorder=10, 
                 label="Sum of two Gaussians")
-        print("widths")
-        print(popt[0], popt[3])
-        print(np.sqrt(pcov[0,0]), np.sqrt(pcov[3,3]))
-
-        print("offsets")
-        print(popt[2], popt[5])
-        print("difference")
-        print((popt[2]-popt[5]))
-        print("error on diff")
-        print(np.sqrt(np.sqrt(pcov[2,2])**2+np.sqrt(pcov[5,5])**2))
 
         if return_residuals:
             ygausmod = two_gaussians(xflare, *popt)
@@ -228,57 +258,324 @@ def ultraspec_gband():
     #plt.show()
 
 
-if __name__=="__main__":
-    # Fit the first LRIS flare
+def lris_gi():
+    """ Fit various functions to the LRIS g+i flares """
+    fig,axarr = plt.subplots(3,2,figsize=(5,6),sharex=True,sharey=True)
+
+    # Get the flare from this day
     out = get_full_opt()
     dat = out[np.logical_and(out['mjdstart']>59866.5, out['mjdstart']<59873)]
-    x = (dat['mjdstart']-dat['mjdstart'].values[0])*24
+    x = (dat['mjdstart']-dat['mjdstart'].values[0])*24*60
     y = dat['flux']
     ey = dat['unc']
 
     # Get the g-band data
-    tofit = np.logical_and(dat['flt']=='g',dat['#instrument']=='KeckI/LRIS') # exponential
-    #tofit = choose # gaussian
+    #tofit = np.logical_and(dat['flt']=='g',dat['#instrument']=='KeckI/LRIS') # exponential
+    tofit = dat['flt']=='g'
     t0 = x[tofit].values[0]
     xgfit = x[tofit]-t0
-    plt.errorbar(x[tofit]-t0, y[tofit], ey[tofit], fmt='o', c='green')
     ygfit = y[tofit]
     eygfit = ey[tofit]
-    plt.errorbar(xgfit, ygfit, eygfit, fmt='o', c='green')
 
-    tofit = np.logical_and(dat['flt']=='i',dat['#instrument']=='KeckI/LRIS') 
+    # Get the i-band data
+    #tofit = np.logical_and(dat['flt']=='i',dat['#instrument']=='KeckI/LRIS')  # exponential
+    tofit = dat['flt']=='i' 
     xifit = x[tofit]-t0
     yifit = y[tofit]
     eyifit = ey[tofit]
-    plt.errorbar(xifit, yifit, eyifit, fmt='o', c='goldenrod')
 
-    # Try a dual-band exponential.
+    for ax in axarr.flatten():
+        ax.errorbar(xifit, yifit, eyifit, fmt='D', c=vals.ic, ms=4)
+        ax.errorbar(xgfit[1:], ygfit[1:], eygfit[1:], fmt='s', c=vals.gc, ms=4)
+        ax.set_yscale('log')
+        ax.set_yticks([2,3,4,6])
+        ax.set_yticklabels([2,3,4,6])
+
+    # Top left: Gaussian fit to the bands separately
+    ax = axarr[0,0]
+    ax.text(0.95, 0.95, 'Gaussian, varying color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    # g-band
+    popt, pcov = curve_fit(
+            gauss, np.array(xgfit).astype(float), 
+            np.array(ygfit).astype(float), 
+            sigma=np.array(eygfit).astype(float),
+            absolute_sigma=True, p0=[40, 4.4, -31], maxfev=10000)
+    xplt = np.linspace(min(xgfit[1:]), max(xgfit))
+    yplt = gauss(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    # i-band
+    xfit_temp = np.append(xifit,-93.6)
+    yfit_temp = np.append(yifit,-7.0)
+    eyfit_temp = np.append(eyifit,4.0)
+    popt, pcov = curve_fit(
+            gauss, np.array(xfit_temp).astype(float), 
+            np.array(yfit_temp).astype(float), 
+            sigma=np.array(eyfit_temp).astype(float),
+            absolute_sigma=True, p0=[40, 6.4, -31], maxfev=10000)
+    xplt = np.linspace(min(xifit), max(xifit))
+    yplt = gauss(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.ic)
+
+    # Top right: Gaussian fit to the bands together
+    ax = axarr[0,1]
+    ax.text(0.95, 0.95, 'Gaussian, constant color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
     xfit = np.hstack((xgfit, xifit)).astype(float)
     yfit = np.hstack((ygfit, yifit)).astype(float)
     eyfit = np.hstack((eygfit, eyifit)).astype(float)
-    p0 = [2, 0.1, 2]
+    p0 = [40, 6.4, -31, 2]
     popt, pcov = curve_fit(
-            exp_gi, xfit, yfit, sigma=eyfit, absolute_sigma=True, p0=p0)
-    xplt = np.linspace(min(xfit), max(xfit))
+            gauss_gi, xfit, yfit, sigma=eyfit, absolute_sigma=True, 
+            p0=p0, maxfev=10000)
+    #popt = p0
+    xplt = np.linspace(min(xfit[1:]), max(xfit))
+    yplt = gauss_gi(np.hstack((xplt,xplt[1:])), *popt)
+    ygplt = yplt[0:int((len(yplt)+1)/2)]
+    yiplt = yplt[int((len(yplt)+1)/2):]
+    ax.plot(xplt, ygplt, c=vals.gc)
+    ax.plot(xplt[1:], yiplt, c=vals.ic)
+
+    # Next row: exponential, single and dual 
+    ax = axarr[1,0]
+    ax.text(0.95, 0.95, 'Exponential, varying color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [0.1, 20]
+    popt, pcov = curve_fit(
+            exp, xgfit[1:], ygfit[1:], sigma=eygfit[1:], 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(xgfit[1:]), max(xgfit))
+    yplt = exp(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+
+    p0 = [0.1, 20]
+    popt, pcov = curve_fit(
+            exp, xifit, yifit, sigma=eyifit, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(xifit), max(xifit))
+    yplt = exp(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.ic)
+
+    # Try a dual-band exponential.
+    ax = axarr[1,1]
+    ax.text(0.95, 0.95, 'Exponential, constant color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [0.1, 20, 2]
+    popt, pcov = curve_fit(
+            exp_gi, xfit[1:], yfit[1:], sigma=eyfit[1:], absolute_sigma=True, p0=p0)
+    xplt = np.linspace(min(xfit[1:]), max(xfit))
     yplt = exp_gi(np.hstack((xplt,xplt)), *popt)
     ygplt = yplt[0:int((len(yplt)+1)/2)]
     yiplt = yplt[int((len(yplt)+1)/2):]
-    plt.plot(xplt, ygplt, c='green')
-    plt.plot(xplt, yiplt, c='yellow')
-    #print(popt, np.sqrt(pcov[1,1]))
-    # Result: 3.6, 0.50, 1.66. clearly doesn't do a good job.
+    ax.plot(xplt, ygplt, c=vals.gc)
+    ax.plot(xplt, yiplt, c=vals.ic)
 
-    # Try a Gaussian
-    #p0 = [0.66, 4.44, 1.05] # for g-band
-    #popt, pcov = curve_fit(
-    #        gauss, xgfit, ygfit, sigma=eygfit, absolute_sigma=True, p0=p0, 
-    #        maxfev=10000)
-    #xplt = np.linspace(-1,2)
-    #yg = gauss(xplt, *popt)
-    #plt.plot(xplt, yg, c='green')
-    #plt.plot(xplt, yg*2, c='yellow')
-    #print(popt)
-    #print(popt[1], np.sqrt(pcov[1,1]))
+    # Try a dual-band Gaussian with an offset
+    ax = axarr[2,1]
+    ax.text(0.95, 0.95, 'Gaussian+offset, constant color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [40, 4, 60, 2, 1] 
+    popt, pcov = curve_fit(
+            gauss_const_gi, xfit[1:], yfit[1:], sigma=eyfit[1:], 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(xfit[1:]), max(xfit))
+    yplt = gauss_const_gi(np.hstack((xplt,xplt)), *popt)
+    ygplt = yplt[0:int((len(yplt)+1)/2)]
+    yiplt = yplt[int((len(yplt)+1)/2):]
+    ax.plot(xplt, ygplt, c=vals.gc)
+    ax.plot(xplt, yiplt, c=vals.ic)
+    print(popt)
+
+    # Gaussian+offset, varying color
+    ax = axarr[2,0]
+    ax.text(0.95, 0.95, 'Gaussian+offset, varying color', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [9, 2, 100, 1.7] 
+    popt, pcov = curve_fit(
+            gauss_const, xgfit[1:], ygfit[1:], sigma=eygfit[1:], 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(xgfit[1:]), max(xgfit))
+    yplt = gauss_const(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+
+    p0 = [9, 2, 100, 1.7] 
+    popt, pcov = curve_fit(
+            gauss_const, xifit, yifit, sigma=eyifit, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(xifit), max(xifit))
+    yplt = gauss_const(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.ic)
+    ax.set_xticklabels([0,5,10,15,20,25]) # shift
+
+    for ax in axarr[:,0]:
+        ax.set_ylabel(r"$f_\nu$ (uJy)")
+    for ax in axarr[-1,:]:
+        ax.set_xlabel("Minutes")
+
+    plt.tight_layout()
+    plt.savefig("lris_gi_fit.png", dpi=200)
+    plt.close()
 
 
+def imacs():
+    """ Fit various functions to the Magellan flares """
+    fig,axarr = plt.subplots(1,3,figsize=(7,2.5),sharex=True,sharey=True)
+
+    # Get the flare from this day
+    out = get_full_opt()
+    dat = out[out['#instrument']=='Magellan/IMACS']
+    x = (dat['mjdstart']-dat['mjdstart'].values[0])*24*60
+    y = dat['flux']
+    ey = dat['unc']
+
+    for ax in axarr.flatten():
+        ax.errorbar(x, y, ey, fmt='s', c=vals.gc, ms=4)
+        #ax.set_yscale('log')
+        #ax.set_yticks([2,3,4,6])
+        #ax.set_yticklabels([2,3,4,6])
+
+    # Top left: Gaussian fit to the bands separately
+    ax = axarr[0]
+    ax.text(0.98, 0.98, 'Gaussian', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    # g-band
+    p0 = [40, 4.4, -31]
+    popt, pcov = curve_fit(gauss, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x))
+    yplt = gauss(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    chisq = sum((y-gauss(x, *popt))**2/ey**2)
+    dof = len(x)-len(p0)
+    red_chisq = chisq/dof
+    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+            ha='right', va='top', transform=ax.transAxes, fontsize=8)
+
+    # Next row: fast rise exponential decay
+    ax = axarr[1]
+    ax.text(0.98, 0.98, 'FRED', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [2E22, 20, -3, 40]
+    popt, pcov = curve_fit(
+            fred, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x), 1000)
+    yplt = fred(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    chisq = sum((y-fred(x, *popt))**2/ey**2)
+    dof = len(x)-len(p0)
+    red_chisq = chisq/dof
+    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+            ha='right', va='top', transform=ax.transAxes, fontsize=8)
+
+    # Gaussian+offset, varying color
+    ax = axarr[2]
+    ax.text(0.98, 0.98, 'Gaussian+offset', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [6, 50, 7, 1.7] 
+    popt, pcov = curve_fit(
+            gauss_const, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x))
+    yplt = gauss_const(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    chisq = sum((y-gauss_const(x, *popt))**2/ey**2)
+    dof = len(x)-len(p0)
+    red_chisq = chisq/dof
+    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+            ha='right', va='top', transform=ax.transAxes, fontsize=8)
+
+    axarr[0].set_ylabel(r"$f_\nu$ (uJy)")
+    for ax in axarr:
+        ax.set_xlabel("Minutes")
+
+    plt.tight_layout()
+    #plt.show()
+
+    # The outcome is that the FRED clearly does the best job.
+    plt.savefig("imacs_fit.png", dpi=200)
+    plt.close()
+
+
+if __name__=="__main__":
+    """ Fit various functions to the LT flare """
+    fig,axarr = plt.subplots(1,3,figsize=(7,2.5),sharex=True,sharey=True)
+
+    # Get the flare from this day
+    out = get_full_opt()
+    dat = out[np.logical_and(out['mjdstart']>59929, out['mjdstart']<59929.9)]
+    x = (dat['mjdstart']-dat['mjdstart'].values[0])*24*60
+    y = dat['flux']
+    ey = dat['unc']
+
+    # Just pick the first few points
+    x = x[0:4]
+    y = y[0:4]
+    ey = ey[0:4]
+
+    for ax in axarr.flatten():
+        ax.errorbar(x, y, ey, fmt='s', c=vals.gc, ms=4)
+
+    # Top left: Gaussian fit 
+    ax = axarr[0]
+    ax.text(0.98, 0.98, 'Gaussian', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    # g-band
+    p0 = [40, 4.4, -31]
+    popt, pcov = curve_fit(gauss, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x))
+    yplt = gauss(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    chisq = sum((y-gauss(x, *popt))**2/ey**2)
+    dof = len(x)-len(p0)
+    red_chisq = chisq/dof
+    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+            ha='right', va='top', transform=ax.transAxes, fontsize=8)
+    print(popt)
+
+    # Next row: fast rise exponential decay
+    ax = axarr[1]
+    ax.text(0.98, 0.98, 'FRED', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [100, 1, 1, 1]
+    popt, pcov = curve_fit(
+            fred, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x), 1000)
+    yplt = fred(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    #chisq = sum((y-fred(x, *popt))**2/ey**2)
+    #dof = len(x)-len(p0)
+    #red_chisq = chisq/dof
+    #ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+    #        ha='right', va='top', transform=ax.transAxes, fontsize=8)
+
+    # Gaussian+offset, varying color
+    ax = axarr[2]
+    ax.text(0.98, 0.98, 'Gaussian+offset', ha='right', va='top',
+            transform=ax.transAxes, fontsize=8)
+    p0 = [4, 11, 3, 1]
+    popt, pcov = curve_fit(
+            gauss_const, x, y, sigma=ey, 
+            absolute_sigma=True, p0=p0, maxfev=10000)
+    xplt = np.linspace(min(x), max(x))
+    yplt = gauss_const(xplt, *popt)
+    ax.plot(xplt, yplt, c=vals.gc)
+    #chisq = sum((y-gauss_const(x, *popt))**2/ey**2)
+    #dof = len(x)-len(p0)
+    #red_chisq = chisq/dof
+    #ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
+    #        ha='right', va='top', transform=ax.transAxes, fontsize=8)
+
+    axarr[0].set_ylabel(r"$f_\nu$ (uJy)")
+    for ax in axarr:
+        ax.set_xlabel("Minutes")
+
+    plt.tight_layout()
     plt.show()
+
+    # The outcome is that the FRED clearly does the best job.
+    #plt.savefig("lt_fit.png", dpi=200)
+    #plt.close()
+
