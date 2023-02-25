72dd38b7f1233f9434c76cb248602aa554dcf52a
Anna Ho <annayqho@gmail.com>
2023-02-25 07:07:36 +0100

    scale bkg

--- code/paper_plots/xray_flares.py
+++ code/paper_plots/xray_flares.py
@@ -1,24 +1,49 @@
 """ Plot the Chandra X-ray flares """
 
+import numpy as np
 import pandas as pd
 from astropy.io import fits as pyfits
 import matplotlib.pyplot as plt
 
 dd = "/Users/annaho/Dropbox/astro/papers/papers_active/AT2022tsd/data/xray"
-fs = ['26641/repro/src2_sub_lc_500s.fits','26642/repro/src2_sub_lc_500s.fits',
-      '26643/repro/src2_sub_lc_500s.fits','26644/repro/src2_sub_lc_500s.fits',
-      '26645/repro/src2_sub_lc_500s.fits','27639/repro/src2_sub_lc_500s.fits',
-      '27643/repro/src2_sub_lc_500s.fits']
+oids = ['26641', '26642', '26643', '26644', '26645', '27639', '27643']
+oids = ['26641', '26642']
 
-f = fs[0]
-tab = pyfits.open(dd+"/"+f)
-dt = tab.get_column('dt').values
-rate = tab.get_column('net_rate').values
-erate = tab.get_column('err_rate').values
-plt.errorbar(
-        dt, rate, yerr=erate, marker="o", color="red", mfc="black",
-        mec="black", ecolor="grey")
+# Initialize the figure...
+fig,axarr = plt.subplots(3,2,figsize=(8,6))
 
+for i,oid in enumerate(oids):
+    ax = axarr.flatten()[i]
+    ff = dd + "/" + oid + "/repro/xray_flare_lc.txt"
+    bf = dd + "/" + oid + "/repro/xray_bkg_lc.txt"
+    ff2 = dd + "/" + oid + "/repro/xray_src_offset_lc.txt"
 
+    dat = np.loadtxt(ff)
+    ax.errorbar(
+            dat[:,0], dat[:,1], yerr=dat[:,2], marker="o", c='k',
+            label='AT2022tsd, background subtracted',zorder=2,ms=5)
+
+    #dat = np.loadtxt(ff2)
+    #ax.errorbar(
+    #        dat[:,0], dat[:,1], yerr=dat[:,2], marker="o", c='grey',
+    #        label='2\'\'-radius nearby region',zorder=0)
+
+    dat = np.loadtxt(bf)
+    ax.plot(dat[:,0], dat[:,1]*(2**2/10**2), c='lightgrey', lw=2, 
+            label='Background, scaled to source-region area', zorder=0)
+    #ax.errorbar(
+    #        dat[:,0], dat[:,1], yerr=dat[:,2], marker="o", c='lightgrey',
+    #        label='Background',zorder=0)
+
+    if i==0:
+        ax.legend(loc='upper left', fontsize=8)
+
+for ax in axarr[:,0]:
+    ax.set_ylabel("Count Rate")
+for ax in axarr[-1,:]:
+    ax.set_xlabel("Seconds")
+
+plt.tight_layout()
+#plt.savefig("first_flare_lc.png", dpi=200)
+#plt.close()
 plt.show()
-
