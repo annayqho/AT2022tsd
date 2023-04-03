d2ea8fb0ae128df1f9471d14b0c258260878d195
Anna Ho <annayqho@gmail.com>
2023-04-03 15:12:13 -0700

    get the ultraspec r-band flare

--- code/fit_opt_flares.py
+++ code/fit_opt_flares.py
@@ -7,8 +7,8 @@ from get_opt import *
 import vals
 
 
-def ultraspec_gband_small(ax, return_residuals=False):
-    """ Fit the small ULTRASPEC g-band flare """
+def ultraspec_gband_small():
+    """ Get the small ULTRASPEC g-band flare """
     out = get_full_opt()
 
     dat = out[np.logical_and(
@@ -17,34 +17,17 @@ def ultraspec_gband_small(ax, return_residuals=False):
     # Choose the first flare
     isflare = dat['isflare']
     t0 = np.min(dat['mjdstart'][isflare])
-    xmin = dat['mjdstart']>t0-0.01
-    xmax = dat['mjdstart']<t0+0.01
+    xmin = dat['mjdstart']>t0-0.006
+    xmax = dat['mjdstart']<t0+0.006
     tofit = np.logical_and(xmin, xmax)
     x = dat['mjdstart'][tofit]*24*60-dat['mjdstart'][tofit].values[0]*24*60
     y = dat['flux'][tofit]
     ey = dat['unc'][tofit]
 
-    # Plot the flare
-    ax.errorbar(x, y, ey, fmt='o', c=vals.gc, ms=4)
-
-    # Fit
-    p0 = [5, 20, 10]
-    popt,pcov = curve_fit(gauss, x, y, sigma=ey, absolute_sigma=True, p0=p0)
-    xplt = np.linspace(min(x), max(x), 1000)
-    yplt = gauss(xplt, *popt)
-    ax.plot(xplt,yplt,lw=1.5,c='k',zorder=10,
-            label=r"$f_\nu \propto e^{-(x-b)^2/(2\sigma^2)}$")
-    ax.text(0.95, 0.95, r"$\sigma=1.5\pm0.1$",ha='right',va='top',fontsize=8,
-            transform=ax.transAxes)
-
-    # Get the residuals
-    ymod = gauss(x, *popt)
-
-    if return_residuals:
-        return x, y-ymod, ey
+    return x.values, y.values, ey.values
 
 
-def ultraspec_gband_large(ax, show='exponential', return_residuals=False):
+def ultraspec_gband_large():
     """ Fit the large ULTRASPEC g-band flare """
     out = get_full_opt()
 
@@ -58,137 +41,34 @@ def ultraspec_gband_large(ax, show='exponential', return_residuals=False):
     # Get flare region
     isflare = dat['isflare']
     t0 = np.max(dat['mjdstart'][isflare])
-    keep = np.logical_and(x>t0-0.048, x<t0+0.1)
-    nofit = np.logical_and(x>t0-0.04, x<t0-0.02)
-    tofit = np.logical_and(keep, ~nofit)
-    xfit = (x[tofit]-x[tofit].values[0])*24*60
-    yfit = dat['flux'][tofit]
-    eyfit = dat['unc'][tofit]
-    xflare = (x[keep]-x[tofit].values[0])*24*60
+    keep = np.logical_and(x>t0-0.05, x<t0+0.5)
+    xflare = (x[keep]-x[keep].values[0])*24*60
     yflare = dat['flux'][keep]
     eyflare = dat['unc'][keep]
+
+    return xflare.values, yflare.values, eyflare.values
     
-    # Fit the exponential
-    p0 = [10, 1]
-    popt,pcov = curve_fit(exp, xfit, yfit, sigma=eyfit, 
-                          absolute_sigma=True, p0=p0)
 
-    if show=='exponential':
-        # Plot the exponential with its best fit
-        plt.errorbar(xfit, yfit, eyfit, fmt='o', c=vals.gc, zorder=2, ms=4)
-        plt.errorbar(
-            xflare, yflare, eyflare, fmt='o', c='lightgrey', zorder=0, ms=4)
-        xplt = np.linspace(min(xfit), max(xfit))
-        yplt = exp(xplt, *popt)
-        plt.plot(
-                xplt,yplt,lw=1.5,c='k',zorder=5,
-                label=r'$f_\nu\propto e^{-t/\tau}$')
-        return popt
-        # Best fit tau: 108 +/- 11 minutes
-
-    elif show=='gaussian':
-        # Divide out the exponential
-        ymod = exp(xflare, *popt)
-        p0 = [1, 2, 18, 1, 2, 30]
-        popt, pcov = curve_fit(two_gaussians,
-                xflare, yflare-ymod, sigma=eyflare/ymod, 
-                absolute_sigma=True, p0=p0)
-        print(popt[1])
-        print(np.sqrt(pcov[1,1]))
-        ygausfit = yflare-ymod
-        plt.errorbar(xflare, ygausfit, yerr=eyflare, fmt='o', 
-                     c=vals.gc, zorder=2, ms=4)
-        xplt = np.linspace(min(xflare), max(xflare), 1000)
-        yplt = two_gaussians(xplt, *popt)
-        ax.plot(
-                xplt,yplt,lw=1.5,c='k', zorder=10, 
-                label="Sum of two Gaussians")
-
-        if return_residuals:
-            ygausmod = two_gaussians(xflare, *popt)
-            return xflare, ygausfit-ygausmod, eyflare
-
-
-def ultraspec_gband():
-    """ Do the analysis for the ULTRASPEC g-band light curve """
-    # Initialize figure
-    fig,axarr = plt.subplots(figsize=(7,7))
-
-    # Plot the full g-band LC
-    ax = plt.subplot(4,1,1)
+def ultraspec_rband():
+    """ get the ULTRASPEC r-band flare """
     out = get_full_opt()
+
+    # Get full LC
     dat = out[np.logical_and(
-        out['#instrument']=='TNT/ULTRASPEC', out['flt']=='g')]
-    x = dat['mjdstart']-dat['mjdstart'].values[0]
+        out['#instrument']=='TNT/ULTRASPEC', out['flt']=='r')]
+    x = dat['mjdstart']
     y = dat['flux']
     ey = dat['unc']
-    ax.errorbar(x*24,y,ey,fmt='o',c=vals.gc,ms=4)
-    ax.axhline(y=0, lw=0.5, c='k', zorder=10)
-    ax.set_xlabel("Time (hours)")
-    ax.set_ylabel("Flux (uJy)")
 
-    # Plot the "noise" in the first 1.5 hours
-    ax = plt.subplot(4,2,3)
-    choose = x*24 < 1.5
-    ax.hist(y[choose],color='grey')
-    ax.set_ylabel("# Points")
-    ax.set_xlabel(r"$f_\nu$")
-    ax.text(0.02, 0.95, "Noise (first 1.5 hrs)", transform=ax.transAxes,va='top',fontsize=8)
-    ax.axvline(x=0, color='k', lw=0.5)
+    # Get flare region
+    isflare = dat['isflare']
+    t0 = np.max(dat['mjdstart'][isflare])
+    keep = np.logical_and(x>t0-0.016, x<t0+0.001)
+    xflare = (x[keep]-x[keep].values[0])*24*60
+    yflare = dat['flux'][keep]
+    eyflare = dat['unc'][keep]
 
-    # Zoom in on the first blip and fit a Gaussian
-    ax = plt.subplot(4,2,5)
-    xres, yres, eyres = ultraspec_gband_small(
-            ax, return_residuals=True) # Gaussian params
-    ax.set_xlabel("Time (mins)")
-    ax.set_ylabel("Flux (uJy)")
-    ax.legend(fontsize=8, loc='upper left')
-
-    # Plot the residuals of the fit
-    ax = plt.subplot(4,2,7)
-    ax.hist(yres, color='grey')
-    ax.text(0.02, 0.95, "Fit Residuals", transform=ax.transAxes,va='top',fontsize=8)
-    ax.axvline(x=0, color='k', lw=0.5)
-    ax.set_xlabel(r"$f_\nu-f_{\nu,\mathrm{model}}$")
-    ax.set_ylabel("# Points")
-
-    # Zoom in on the second obvious flare and fit an exponential
-    ax = plt.subplot(4,2,4)
-    ultraspec_gband_large(ax) # exponential params
-    ax.set_xlabel("Time (mins)")
-    ax.set_ylabel("Flux (uJy)")
-    ax.legend(fontsize=8, loc='upper right')
-    ax.text(0.05,0.05,r'$\tau=110\pm11$ mins',transform=ax.transAxes, fontsize=8)
-    #plt.axhline(y=1, lw=0.5, c='k')
-
-    # Divide the exponential out and fit a double Gaussian
-    ax = plt.subplot(4,2,6)
-    xres, yres, eyres = ultraspec_gband_large(
-            ax, show='gaussian', return_residuals=True)
-    ax.legend(loc='upper right', fontsize=8)
-    ax.text(
-            0.02, 0.03, 
-            '$\sigma_1=2.3$ mins, $\sigma_2=3.7$ mins, sep=$11.4$ mins',
-            transform=ax.transAxes, fontsize=8)
-    ax.set_ylim(-9,16)
-    ax.set_xlabel("Time (mins)")
-    ax.set_ylabel("Flux (uJy)")
-
-    # Plot the residuals of the fit
-    ax = plt.subplot(4,2,8)
-    ax.hist(yres, color='grey')
-    ax.text(0.02, 0.95, "Fit Residuals", transform=ax.transAxes,va='top',fontsize=8)
-    ax.axvline(x=0, color='k', lw=0.5)
-    ax.set_xlabel(r"$f_\nu-f_{\nu,\mathrm{model}}$")
-    ax.set_ylabel("# Points")
-
-    # Show
-    plt.tight_layout()
-    plt.savefig(
-            "ultraspec_flare_fits.png", dpi=200, 
-            bbox_inches='tight', pad_inches=0.1)
-    plt.close()
-    #plt.show()
+    return xflare.values, yflare.values, eyflare.values
 
 
 def lris_gi():
@@ -390,169 +270,76 @@ def lris_gi():
 
 def imacs():
     """ Fit various functions to the Magellan flares """
-    fig,axarr = plt.subplots(1,3,figsize=(7,2.5),sharex=True,sharey=True)
-
     # Get the flare from this day
     out = get_full_opt()
     dat = out[out['#instrument']=='Magellan/IMACS']
     x = (dat['mjdstart']-dat['mjdstart'].values[0])*24*60
-    y = dat['flux']
-    ey = dat['unc']
-
-    for ax in axarr.flatten():
-        ax.errorbar(x, y, ey, fmt='s', c=vals.gc, ms=4)
-        #ax.set_yscale('log')
-        #ax.set_yticks([2,3,4,6])
-        #ax.set_yticklabels([2,3,4,6])
-
-    # Top left: Gaussian fit to the bands separately
-    ax = axarr[0]
-    ax.text(0.98, 0.98, 'Gaussian', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    # g-band
-    p0 = [40, 4.4, -31]
-    popt, pcov = curve_fit(gauss, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x))
-    yplt = gauss(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    chisq = sum((y-gauss(x, *popt))**2/ey**2)
-    dof = len(x)-len(p0)
-    red_chisq = chisq/dof
-    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-            ha='right', va='top', transform=ax.transAxes, fontsize=8)
-
-    # Next row: fast rise exponential decay
-    ax = axarr[1]
-    ax.text(0.98, 0.98, 'FRED', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    p0 = [2E22, 20, -3, 40]
-    popt, pcov = curve_fit(
-            fred, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x), 1000)
-    yplt = fred(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    chisq = sum((y-fred(x, *popt))**2/ey**2)
-    dof = len(x)-len(p0)
-    red_chisq = chisq/dof
-    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-            ha='right', va='top', transform=ax.transAxes, fontsize=8)
-    print(popt[3], np.sqrt(pcov[3,3]))
-
-    # Gaussian+offset, varying color
-    ax = axarr[2]
-    ax.text(0.98, 0.98, 'Gaussian+offset', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    p0 = [6, 50, 7, 1.7] 
-    popt, pcov = curve_fit(
-            gauss_const, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x))
-    yplt = gauss_const(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    chisq = sum((y-gauss_const(x, *popt))**2/ey**2)
-    dof = len(x)-len(p0)
-    red_chisq = chisq/dof
-    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-            ha='right', va='top', transform=ax.transAxes, fontsize=8)
-
-    axarr[0].set_ylabel(r"$f_\nu$ (uJy)")
-    for ax in axarr:
-        ax.set_xlabel("Minutes")
-
-    plt.tight_layout()
-    plt.show()
-
-    # The outcome is that the FRED clearly does the best job.
-    #plt.savefig("imacs_fit.png", dpi=200)
-    #plt.close()
+    y = dat['flux_extcorr']
+    ey = dat['unc_extcorr']
+    return x.values, y.values, ey.values
 
 
 def lt():
-    """ Fit various functions to the LT flare """
-    fig,axarr = plt.subplots(1,3,figsize=(7,2.5),sharex=True,sharey=True)
-
-    # Get the flare from this day
+    """ Retrieve the extinction-corrected LT flare """
     out = get_full_opt()
     dat = out[np.logical_and(out['mjdstart']>59929, out['mjdstart']<59929.9)]
     x = (dat['mjdstart']-dat['mjdstart'].values[0])*24*60
-    y = dat['flux']
-    ey = dat['unc']
+    y = dat['flux_extcorr']
+    ey = dat['unc_extcorr']
+    filt = dat['flt']
+    return filt.values, x.values, y.values, ey.values
 
-    print(max(x)-min(x))
 
-    # Just pick the first few points
-    x = x[0:4]
-    y = y[0:4]
-    ey = ey[0:4]
+def calc_t90(nu, x, y, ey):
+    """ Calculate the T90 of a flare, aka the time in which 
+    90% of the flux was recorded 
 
-    for ax in axarr.flatten():
-        ax.errorbar(x, y, ey, fmt='s', c=vals.gc, ms=4)
+    Parameters
+    ----------
+    f: filter
+    x: time values
+    y: flux
+    ey: uncertainty on flux
+    """
 
-    # Top left: Gaussian fit 
-    ax = axarr[0]
-    ax.text(0.98, 0.98, 'Gaussian', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    # g-band
-    p0 = [40, 4.4, -31]
-    popt, pcov = curve_fit(gauss, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x))
-    yplt = gauss(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    chisq = sum((y-gauss(x, *popt))**2/ey**2)
-    dof = len(x)-len(p0)
-    red_chisq = chisq/dof
-    ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-            ha='right', va='top', transform=ax.transAxes, fontsize=8)
+    # First, get the peak luminosity
+    ind = np.argmax(y)
+    Lpeak = y[ind]*1E-6*1E-23*4*np.pi*vals.dL_cm**2 * nu
 
-    # Next row: fast rise exponential decay
-    ax = axarr[1]
-    ax.text(0.98, 0.98, 'FRED', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    p0 = [100, 1, 1, 1]
-    popt, pcov = curve_fit(
-            fred, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x), 1000)
-    yplt = fred(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    #chisq = sum((y-fred(x, *popt))**2/ey**2)
-    #dof = len(x)-len(p0)
-    #red_chisq = chisq/dof
-    #ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-    #        ha='right', va='top', transform=ax.transAxes, fontsize=8)
+    # Interpolate onto a grid
+    xvals = np.linspace(min(x), max(x), 1000)
+    yvals = np.interp(xvals, x, y)
 
-    # Gaussian+offset, varying color
-    ax = axarr[2]
-    ax.text(0.98, 0.98, 'Gaussian+offset', ha='right', va='top',
-            transform=ax.transAxes, fontsize=8)
-    p0 = [4, 11, 3, 1]
-    popt, pcov = curve_fit(
-            gauss_const, x, y, sigma=ey, 
-            absolute_sigma=True, p0=p0, maxfev=10000)
-    xplt = np.linspace(min(x), max(x))
-    yplt = gauss_const(xplt, *popt)
-    ax.plot(xplt, yplt, c=vals.gc)
-    #chisq = sum((y-gauss_const(x, *popt))**2/ey**2)
-    #dof = len(x)-len(p0)
-    #red_chisq = chisq/dof
-    #ax.text(0.98, 0.92, "$\chi^2$=%s" %np.round(red_chisq,1), 
-    #        ha='right', va='top', transform=ax.transAxes, fontsize=8)
+    # Get the cumulative fluence
+    total = np.cumsum(yvals)[-1]
 
-    axarr[0].set_ylabel(r"$f_\nu$ (uJy)")
-    for ax in axarr:
-        ax.set_xlabel("Minutes")
+    # Fractional light curve as a function of time
+    fvals = np.cumsum(yvals)/total
 
-    plt.tight_layout()
+    # Identify where you cross 5% for the last time and 95% for the first time
+    tstart= xvals[np.where(fvals<0.05)[0][-1]]
+    tend = xvals[np.where(fvals>0.95)[0][0]]
+
+    # Calculate T90
+    t90 = tend-tstart
+    print(t90, Lpeak)
+    plt.plot(xvals, fvals, c='grey')
+    plt.axvline(x=tstart, c='k')
+    plt.axvline(x=tend, c='k')
     plt.show()
 
-
-def calc_t90():
-    """ Calculate the T90 of a flare, aka the time in which 
-    90% of the flux was recorded """
+    return t90, Lpeak
 
 
 if __name__=="__main__":
-    calc_t90()
+    #f, x, y, ey = lt()
+    #x, y, ey = imacs()
+    #x, y, ey = ultraspec_gband_large()
+    x, y, ey = ultraspec_rband()
+    nu = 3E18/vals.ztf_pivot['r']
+    t90, lpeak = calc_t90(nu, x, y, ey) 
+    # LT: 10.4 minutes, 3.6E43 erg/s
+    # IMACS: 16.1 minutes, 1.5E44 erg/s
+    # ULTRASPEC g-band, short: 7.7 minutes. 1.8E43 erg/s
+    # ULTRASPEC g-band, large: 78.0 minutes, 2.7E43 erg/s
+    # ULTRASPEC r-band: 19.1 minutes, 3.0E43 erg/s
