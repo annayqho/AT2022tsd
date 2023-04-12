a2d2dde887b98360a87ab5aac96dc66ea6254839
Anna Ho <annayqho@gmail.com>
2023-04-12 14:30:12 -0400

    simplify functions

--- code/get_spec.py
+++ code/get_spec.py
@@ -13,28 +13,30 @@ def load_smoothed_spec(x, y, ivar):
     return smoothed
 
 
-def load_spec_1():
-    """ read in the spectrum """
-    inputf = "%s/opt/LRIS/lris20220923_improved_redux.spec" %ddir
-    tab = pd.read_fwf(inputf, skiprows=np.arange(147))
+def load_spec(inputf, ran):
+    tab = pd.read_fwf(inputf, skiprows=np.arange(ran))
     wl = tab['# wavelen'].values # AA
     # erg/cm2/s/Ang, absolute calibration approximate
     flam = tab['flux'].values 
-    eflam[flam<0] = 1E8
-    flam[flam<0] = 0
     eflam = tab['flux_unc'].values
+
+    # Set bad values to 0, with large errors
+    eflam[flam<0] = 1E8
+    eflam[np.isnan(flam)] = 1E8
+    flam[flam<0] = 0
+    flam[np.isnan(flam)] = 0
+    return wl, flam, eflam
+
+
+def load_spec_1():
+    """ read in the spectrum """
+    inputf = "%s/opt/LRIS/lris20220923_improved_redux.spec" %ddir
+    wl, flam, eflam = load_spec(inputf, 147)
     return wl, flam, eflam
 
 
 def load_spec_2():
     """ read in the spectrum """
-    #inputf = "%s/opt/LRIS/ZTF22abftjko_20221006_Keck1_v1.ascii" %ddir
     inputf = "%s/opt/LRIS/lris20221006_adjust.spec" %ddir
-    tab = pd.read_fwf(inputf, skiprows=np.arange(150))
-    wl = tab['wavelen'].values # AA
-    # erg/cm2/s/Ang, absolute calibration approximate
-    flam = tab['flux'].values 
-    eflam[flam<0] = 1E8
-    flam[flam<0] = 0
-    eflam = tab['flux_unc'].values
+    wl, flam, eflam = load_spec(inputf, 150)
     return wl, flam, eflam
