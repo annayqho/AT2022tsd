f6eaa05e2d303c5e51fc0ca81d42918b6a08b26e
Anna Ho <annayqho@gmail.com>
2023-04-21 13:10:38 -0400

    search for periodicity between flares

--- code/opt_period_search_all_flares.py
+++ code/opt_period_search_all_flares.py
@@ -1,4 +1,4 @@
-from astropy.timeseries import LombScargle
+#from astropy.timeseries import LombScargle
 import matplotlib.pyplot as plt
 import cmasher as cmr
 from get_opt import *
@@ -15,33 +15,35 @@ isf = dat['isflare'][choose].values
 filt = dat['flt'][choose].values
 tel = dat['#instrument'].values[choose]
 
-ls = LombScargle(t, f, ef)
-frequency, power = ls.autopower(method='slow', samples_per_peak=5)
-period = 1/frequency
+#ls = LombScargle(t, f, ef)
+#frequency, power = ls.autopower(method='slow', samples_per_peak=5)
+#period = 1/frequency
 #plot(period, power, 'k')
 
-period_sorted = period[np.argsort(power)][::-1]
+#period_sorted = period[np.argsort(power)][::-1]
+period_sorted = np.arange(3/24, 1, 5/60/24)
 
 cols = cmr.take_cmap_colors(
-    'cmr.rainforest', 9, cmap_range=(0.1, 0.9), return_fmt='hex')[::-1]
-m = ['o', 'D', 's', '*', 'v', '>', '<', '^', 'H']
+    'cmr.rainforest', 11, cmap_range=(0.1, 0.9), return_fmt='hex')[::-1]
+m = ['o', 'D', 's', '*', 'v', '>', '<', '^', 'H', 'o', 'D']
 
-
-#for i in np.arange(0,50):
-for i in [22]:
+for i in np.arange(len(period_sorted)):
     fig,ax = plt.subplots(1,1,figsize=(8,4))
     P = period_sorted[i]
+    print(P)
 
     for j,tel_val in enumerate(np.unique(tel)):
         choose = np.logical_and(tel==tel_val, isf)
-        plt.errorbar(t[choose]/P % 1, f[choose], ef[choose], fmt=m[j], ms=4,label=tel_val,
+        plt.errorbar(t[choose]/P % 1, f[choose], ef[choose], fmt=m[j], 
+                     ms=4,label=tel_val,
                      c=cols[j], zorder=2)
         #choose = np.logical_and(tel==tel_val, ~isf)
         #plt.errorbar(t[choose]/P % 1, f[choose], ef[choose], fmt=m[j], ms=4,alpha=0.1,
         #             c=cols[j], zorder=0)
     plt.title(P*24)
     plt.legend()
-    plt.savefig("P_only_flare_period_%s_det_only.png" %(i))
+    plt.xlim(-0.1,1.1)
+    plt.savefig("period_search_3hr_1d_5minstep/P_only_flare_period_%s.png" %(i))
     plt.close()
 
     # Tentative period at 5.88 hours?
