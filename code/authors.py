7e48fa1081f10f2992b1f749d82cf5039eef3cbe
Anna Ho <annayqho@gmail.com>
2023-05-08 11:51:14 -0400

    print affiliations list

--- code/authors.py
+++ code/authors.py
@@ -6,6 +6,9 @@ import pandas as pd
 # Load data
 dat = pd.read_excel("../data/coauthors.xlsx")
 
+# Add column for surname
+dat['Surname'] = [val.split(' ')[-1] for val in dat['Name'].values]
+
 ### Put all the affiliations in the correct order in the array
 affs = []
 
@@ -13,19 +16,49 @@ affs = []
 ordered = dat[~np.isnan(dat['Order'].values)].sort_values('Order').reset_index()
 for index,row in ordered.iterrows():
     for i in [1,2,3]:
-        affil = dat['Affil%s'%i].values
+        affil = row['Affil%s'%i]
         affs.append(affil)
 
 # Next, go through the rest of the co-authors
-alphabetical = dat[np.isnan(dat['Order'].values)].sort_values('Name').reset_index()
-for index,row in ordered.iterrows():
+alphabetical = dat[np.isnan(dat['Order'].values)].sort_values('Surname').reset_index()
+for index,row in alphabetical.iterrows():
     for i in [1,2,3]:
-        affil = dat['Affil%s'%i].values
+        affil = row['Affil%s'%i]
         affs.append(affil)
 
 # Remove nans
-aff = np.array(aff).astype(str)
-aff = aff[aff!='nan']
+affs = np.array(affs).astype(str)
+affs = affs[affs!='nan']
 # Make unique array
-aff = np.unique(aff)
+_,idx = np.unique(affs, return_index=True)
+affs_unique = affs[np.sort(idx)]
     
+# Now, go through authors in order and give exponent of affil #
+authorstr = ""
+for index,row in ordered.iterrows():
+    name = row['Name']
+    authorstr += "%s$^{"%(name)
+    for i in [1,2,3]:
+        affil = row['Affil%s'%i]
+        if pd.isnull(affil)==False:
+            ind = np.where(affs_unique==affil)[0][0]
+            authorstr += "%s,"%(ind+1)
+    authorstr = authorstr[:-1]
+    authorstr += "}$, "
+
+for index,row in alphabetical.iterrows():
+    name = row['Name']
+    authorstr += "%s$^{"%(name)
+    for i in [1,2,3]:
+        affil = row['Affil%s'%i]
+        if pd.isnull(affil)==False:
+            ind = np.where(affs_unique==affil)[0][0]
+            authorstr += "%s,"%(ind+1)
+    authorstr = authorstr[:-1]
+    authorstr += "}$, "
+authorstr = authorstr[:-2]
+# Have to remove triple backslashes by hand
+
+# Produce affiliation string
+for aff in affs_unique:
+    print("\\item %s" %aff)
