abd1b0441d3744e2bcb27d07c4e09fd907680d33
Anna Ho <annayqho@gmail.com>
2023-07-24 11:11:43 -0400

    correct the equipartition calculations

--- code/calc/calc_synchrotron.py
+++ code/calc/calc_synchrotron.py
@@ -1,54 +1,68 @@
 """ Synchrotron calculations 
 
-Following Fender et al. (1997) for GRS 1915+105
-and Readhead synchrotron notes
+Following Readhead synchrotron notes
 """
 
 import numpy as np
 
-# Input: Fender values
-# R = 9E12
-# L = 1E36
-# alpha = 0
-# nu1 = 1E14
-# nu2 = 15*1E9
-# obs_nu = 1.4E14
-
-Gamma = 1
-
-# Input: our values
-R = 1.4E12 * Gamma**2
-L = 1E43
-alpha = -1.6
-nu1 = 1E15
-nu2 = 1E13
-obs_nu = 1E15
-
 # Constants
 A = 1.586E12
 
-# Radius: 1 light-minute, which is 10^12 cm
-V = (4/3) * np.pi * R**3
+def get_V(R):
+    V = (4/3) * np.pi * R**3
+    return V
 
-# Values of g...uncertain here
-g = ((2*alpha+2)/(2*alpha+1)) * ((nu2**(alpha+1/2)-nu1**(alpha+1/2))/(nu2**(alpha+1)-nu1**(alpha+1)))
 
-# Equiparititon magnetic field
-B = (8*np.pi*A*g*L/V)**(2/7)
-print(B)
+def get_g(alpha,nu1,nu2):
+    g_first_term = (2*alpha+2)/(2*alpha+1)
+    g_second_term_top = nu2**(alpha+1/2)-nu1**(alpha+1/2)
+    g_second_term_bottom = nu2**(alpha+1)-nu1**(alpha+1)
+    g = g_first_term * (g_second_term_top / g_second_term_bottom)
+    return g
 
-# Eqiuparittion energy
-U = V*B**2/(8*np.pi) + A*g*L*B**(-3/2)
-print(U)
 
-# For a given magnetic field strength, what is the required particle energy
-# at optical frequencies, say 1E15 Hz?
-nu_g = (2.8 / B)*1E6 # nu_g = 2.8 MHz / Gauss; this value is in Hz
-gamma_e = np.sqrt((obs_nu/nu_g))
-print(gamma_e/1E6)
-particle_energy_erg = gamma_e * 9E-28 * (3E10)**2
-particle_energy_eV = particle_energy_erg*6.242E11
-#print(particle_energy_eV/1E9)
+def Beq(R, L, alpha, nu1, nu2):
+    """ Equipartition magnetic field """
+    V = get_V(R)
+    g = get_g(alpha, nu1, nu2)
+    B = (8*np.pi*A*g*L/V)**(2/7)
+    return B
 
-tcool = 6*np.pi*9E-28*3E10 / (6.65E-25 * B**2 * gamma_e)
-#print(tcool) # cooling time in hours
+
+def Ueq(R, L, alpha, nu1, nu2):
+    V = get_V(R)
+    g = get_g(alpha, nu1, nu2)
+    B = Beq(R, L, alpha, nu1, nu2)
+    U = 2 * V*B**2/(8*np.pi) # + A*g*L*B**(-3/2)
+    return U
+
+
+def calc_gamma_e(B, obs_nu):
+    # CONSTANTS
+    c = 3E10
+    q_e = 4.8E-10
+    m_e = 9.1E-28
+
+    nu_g = q_e * B / (2 * np.pi * m_e * c)
+    gamma_e = np.sqrt((obs_nu/nu_g))
+    return gamma_e
+
+
+if __name__=="__main__":
+    # Lorentz factor
+    Gamma = 1
+
+    # Input: our values
+    R = 9E11 * Gamma**2
+    L = 1E43
+    alpha = -1.6
+    nu1 = 1E13
+    nu2 = 1E15
+    obs_nu = 1E15 # Hz
+
+    B = Beq(R, L, alpha, nu1, nu2)
+    U = Ueq(R, L, alpha, nu1, nu2)
+    gamma_e = calc_gamma_e(B, obs_nu)
+
+    tcool = 6*np.pi*9E-28*3E10 / (6.65E-25 * B**2 * gamma_e)
+    #print(tcool) # cooling time in hours
